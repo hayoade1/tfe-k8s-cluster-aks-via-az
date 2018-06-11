e6e948e3cbeb632e97815f9a316aa7e5b414ae72
Roger Berlind <roger@hashicorp.com>
2018-06-11 14:53:20 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -16,7 +16,7 @@ resource "null_resource" "install_az" {
     command = "echo 'deb [arch=amd64] https://packages.microsoft.com/repos/azure-cli/ wheezy main' | sudo tee /etc/apt/sources.list.d/azure-cli.list"
   }
   provisioner "local-exec" {
-    command = "sudo apt-key adv --keyserver packages.microsoft.com --recv-keys 52E16F86FEE04B979B07E28DB02C46DF417A0893"
+    command = "curl -L https://packages.microsoft.com/keys/microsoft.asc | sudo apt-key add -"
   }
   provisioner "local-exec" {
     command = "sudo apt-get -y install apt-transport-https"
