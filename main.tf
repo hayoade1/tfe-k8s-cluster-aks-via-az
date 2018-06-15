45bd7f17751a4c69335396e9f8658029a44a6c2b
Roger Berlind <roger@hashicorp.com>
2018-06-15 15:35:22 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -13,7 +13,7 @@ resource "azurerm_resource_group" "k8sexample" {
 
 resource "null_resource" "install_az" {
   provisioner "local-exec" {
-    command = "echo 'deb [arch=amd64] https://packages.microsoft.com/repos/azure-cli/ $(lsb_release -cs) main' | sudo tee /etc/apt/sources.list.d/azure-cli.list"
+    command = "echo \"deb [arch=amd64] https://packages.microsoft.com/repos/azure-cli/ $(lsb_release -cs) main\" | sudo tee /etc/apt/sources.list.d/azure-cli.list"
   }
   provisioner "local-exec" {
     command = "curl -L https://packages.microsoft.com/keys/microsoft.asc | sudo apt-key add -"
